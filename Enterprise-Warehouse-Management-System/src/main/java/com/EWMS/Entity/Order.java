b77533c4580c87f4d519bb7c7e2eb0e5faf0578e
yasmeenguled7815-bit <yasmeenguled7815@gmail.com>
2026-06-03 14:42:51 +0530

    added commit

--- Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Entity/Order.java
+++ Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Entity/Order.java
@@ -1,5 +1,61 @@
 package com.EWMS.Entity;
 
+import Enum.OrderStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Order {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private Integer quantity;
+	
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+	
+	@ManyToOne
+	private Product product;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	
+	
 
 }
