82d86e0ae7b3fc73a86b0318ca3d78c3f28824a3
yasmeenguled7815-bit <yasmeenguled7815@gmail.com>
2026-06-05 16:44:48 +0530

    added Order controller

--- Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Controller/OrderController.java
+++ Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Controller/OrderController.java
@@ -1,12 +1,15 @@
 package com.EWMS.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.EWMS.Entity.Order;
+import com.EWMS.Enum.OrderStatus;
 import com.EWMS.service.OrderService;
 
 @RestController
@@ -21,7 +24,10 @@ public class OrderController {
 		return orderService.createOrder(order);
 	}
 	
-	
+	public Order updateOrderStstus(@PathVariable Long id, @RequestParam OrderStatus status) {
+		
+		return orderService.updateStatus(id, status);
+	}
 	
 	
 
