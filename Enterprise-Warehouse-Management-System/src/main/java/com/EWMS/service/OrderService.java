6ecdea381f768c15ff7739b7bf60e302ff2d22a8
yasmeenguled7815-bit <yasmeenguled7815@gmail.com>
2026-06-05 16:16:38 +0530

    added order service

--- Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/service/OrderService.java
+++ Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/service/OrderService.java
@@ -12,21 +12,33 @@ import com.EWMS.exception.InsufficientStockException;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private InventoryItemRepository inventoryItemRepository;
-	
-public Order updateStatus(Long orderId, OrderStatus status) {
-		
-		Order order=orderRepository.findById(orderId)
+
+	public Order updateStatus(Long orderId, OrderStatus status) {
+
+		Order order = orderRepository.findById(orderId)
 				.orElseThrow(() -> new RuntimeException("Order not found"));
-			
-			order.setStatus(status);
-			return orderRepository.save(order);
+
+		if (status == OrderStatus.PACKED) {
+
+			InventoryItem inventory = inventoryItemRepository.findByProductId(order.getProduct().getId())
+					.orElseThrow(() -> new RuntimeException("Inventory items not found"));
+
+			if (inventory.getQuantity() < order.getQuantity()) {
+				throw new InsufficientStockException("Not enough stock available");
+			}
+
+			inventory.setQuantity(inventory.getQuantity() - order.getQuantity());
+
+			inventoryItemRepository.save(inventory);
 		}
+
+		order.setStatus(status);
+		return orderRepository.save(order);
 	}
-	
-	
+}
