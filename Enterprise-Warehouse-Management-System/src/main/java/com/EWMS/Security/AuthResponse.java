53c59e7c013f1a534e7c0863d2e27c9bbb2e2aff
yasmeenguled7815-bit <yasmeenguled7815@gmail.com>
2026-06-12 11:00:18 +0530

    updated authrasponse

--- Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Security/AuthResponse.java
+++ Enterprise-Warehouse-Management-System/src/main/java/com/EWMS/Security/AuthResponse.java
@@ -1,5 +1,19 @@
 package com.EWMS.Security;
 
 public class AuthResponse {
+	
+	    private String token;
 
-}
+	    public AuthResponse(String token) {
+	        this.token = token;
+	    }
+
+	    public String getToken() {
+	        return token;
+	    }
+
+	    public void setToken(String token) {
+	        this.token = token;
+	    }
+	}
+
